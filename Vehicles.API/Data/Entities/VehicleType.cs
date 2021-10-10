d987722763192c0b20a31b4d074a241a79409c51
walter2016 <walteralex1992@gmail.com>
2021-10-10 12:48:25 -0600

    Adicionar el VehicleType

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int id
+        {
+            get;
+            set;
+        }
+
+        [Display(Name = "Tipo de vehículo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Description { get; set; }
+    }
+}
